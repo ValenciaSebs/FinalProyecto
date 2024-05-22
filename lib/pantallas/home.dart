731a359f2ae7ce2188d7824a7500b3b973be60d1
sebas27rin <sebas27rin@gmail.com>
2024-05-22 23:57:28 +0000

    comiit inicial del proyecto, aaah Diossss

---
+++ lib/pantallas/home.dart
@@ -0,0 +1,21 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:662371213.
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3162623435.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2582425092.
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home bueno'),
+        ),
+        body: const Center(
+          child: Text('Home de prueba buen'),
+        ),
+      )
+    );
+  }
+}
